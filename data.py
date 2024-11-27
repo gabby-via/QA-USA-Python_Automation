1e11682cd59dcd0a098b2a3a5f57ecd5230b2803
gabby-via <jajacs502020@gmail.com>
2024-11-27 16:14:49 -0500

    Sprint 7 Project commit

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 # File with constants
+URBAN_ROUTES_URL = "https://cnt-221864be-f711-4c8d-9e7f-11c4eae93132.containerhub.tripleten-services.com"
+TARIFFS = ('Business', 'Sleepy', 'Holiday', 'Talking', 'Supportive', 'Glossy')
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE =  '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,69 @@
+import data
+import helpers
+from helpers import is_url_reachable
+from data import URBAN_ROUTES_URL
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class (cls):
+        if is_url_reachable(URBAN_ROUTES_URL):
+            #task4
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+
+    def test_set_route(self):
+     # Add in S8
+        print("Function created for set route")
+        pass  # placeholder for future implementation
+
+
+    def test_select_plan (self):
+     # Add in S8
+        print("Function created for set route")
+        pass # placeholder for future implementation
+
+
+
+    def test_fill_phone_number (self):
+     # Add in S8
+        print ("Function created for set route")
+        pass #placeholder for future implementation
+
+
+    def test_fill_card (self):
+     # Add in S8
+        print ("Function created for set route")
+        pass # placeholder for future implementation
+
+
+    def test_comment_for_driver (self):
+      # Add in S8
+        print ("Function created for set route")
+        pass # placeholder for future implementation
+
+
+
+    def test_order_blanket_and_handkerchiefs (self):
+      # Add in S8
+        print ("Function created for set route")
+        pass #placeholder for future implementation
+
+
+
+    def test_order_2_ice_creams (self):
+     # Add in S8
+        print ("Function created for set route")
+        pass #placeholder for future implementation
+        for i in range(2):
+            #task5
+            # Add in S8
+            pass
+
+
+    def test_car_search_model_appears (self):
+     # Add in S8
+        print ("Function created for set route")
+        pass #placeholder for future implementation
